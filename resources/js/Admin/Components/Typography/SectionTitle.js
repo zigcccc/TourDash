3d258cdf96ee3b0dc47e49bf1061a951e09f1546
Žiga Krašovec <zkrasovec@gmail.com>
2018-12-15 00:16:03 +0100

    :recycle: (admin-frontend) Created global Typography group of components used accross all admin ui …

---
+++ resources/js/Admin/Components/Typography/SectionTitle.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Title = styled.h2`
+	font-size: 42px;
+	font-weight: 900;
+	color: ${props => props.theme.darkPrimary};
+`;
+
+const SectionTitle = props => {
+	return <Title>{props.text}</Title>;
+};
+
+SectionTitle.propTypes = {
+	text: PropTypes.string.isRequired
+};
+
+export default SectionTitle;
