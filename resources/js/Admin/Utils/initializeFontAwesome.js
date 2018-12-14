8ebc87b07820003ec8bc172919ad5ab1a1a1a891
Žiga Krašovec <zkrasovec@gmail.com>
2018-12-15 00:14:07 +0100

    :recycle: (global-frontend) Moved FontAwesome initialization into separate file since it became qui…

---
+++ resources/js/Admin/Utils/initializeFontAwesome.js
@@ -0,0 +1,63 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+	faChevronLeft,
+	faPlus,
+	faMinus,
+	faTh,
+	faChevronRight,
+	faChevronDown,
+	faChevronUp,
+	faUser,
+	faUnlock,
+	faCircleNotch,
+	faLongArrowAltRight,
+	faLongArrowAltLeft,
+	faRedoAlt,
+	faSearch,
+	faSignOutAlt,
+	faEnvelope,
+	faEllipsisV,
+	faPencilAlt,
+	faFile,
+	faHome,
+	faCalendarAlt,
+	faThumbtack,
+	faHeart,
+	faStar,
+	faTrashAlt,
+	faStarHalfAlt,
+	faEyeSlash
+} from "@fortawesome/free-solid-svg-icons";
+
+const initializeFontAwesome = () =>
+	library.add(
+		faChevronLeft,
+		faPlus,
+		faMinus,
+		faTh,
+		faChevronRight,
+		faUser,
+		faUnlock,
+		faCircleNotch,
+		faLongArrowAltRight,
+		faLongArrowAltLeft,
+		faRedoAlt,
+		faChevronDown,
+		faChevronUp,
+		faSearch,
+		faSignOutAlt,
+		faEnvelope,
+		faEllipsisV,
+		faPencilAlt,
+		faFile,
+		faHome,
+		faCalendarAlt,
+		faThumbtack,
+		faHeart,
+		faStar,
+		faTrashAlt,
+		faStarHalfAlt,
+		faEyeSlash
+	);
+
+export default initializeFontAwesome;
